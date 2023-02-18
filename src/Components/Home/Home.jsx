688f63e551fc4ece161241ec16e4fe1a90271717
siddharth200119 <siddramnani@gmail.com>
2023-02-19 00:59:29 +0530

    added motion for cloud gotta finish later

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./Home.css"
+import {motion} from "framer-motion"
 
 function Home(){
     return(
-        <div className='home'>
-            <h1>hello</h1>
+        <div className='sky'>
+            <motion.div 
+            animate={{
+                x: window.screen.width,
+            }}
+            initial={{
+                x: 0,
+            }}
+            transition={{
+                duration: 5,
+                repeat: Infinity,
+                repeatType: "reverse",
+                repeatDelay: 1,
+            }}
+            >
+                <img className='cloud' src={process.env.PUBLIC_URL + "Cloud.png"}></img>
+            </motion.div>
+
+            <h1>test</h1>
+            
         </div>
     )
 }
