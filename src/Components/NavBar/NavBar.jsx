a153dc3f3f4b486759285c3b87107a4f6be26df2
siddharth200119 <siddramnani@gmail.com>
2023-08-02 00:12:12 +0530

    changed navbar color on scroll

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ function NavBar(){
     useEffect(() => {
       if (typeof window !== "undefined") {
         window.addEventListener("scroll", () =>
-          setHide(window.scrollY < window.innerHeight * 0.5)
+          setHide(window.scrollY < window.innerHeight * 0.65)
         );
       }
     }, []);
@@ -22,7 +22,7 @@ function NavBar(){
             </div>
             <div className='NavBarItems'>
                 {NavBarItems.map((item) => (
-                    <a href={'#' + item.title} className='NavBarItem'>{item.title.charAt(0).toUpperCase() + item.title.slice(1)} </a>
+                    <a href={'#' + item.title} className={`NavBarItem ${hide ? "scrolled_color" : ""}`}>{item.title.charAt(0).toUpperCase() + item.title.slice(1)} </a>
                 ))}
             </div>
             <div className='links'>
